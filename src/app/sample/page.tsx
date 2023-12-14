f80923b03dac03a336a6e3148b211d15de9badb2
taro710 <katayamayutaro@team-lab.com>
2023-12-14 22:43:14 +0900

    feat: 環境構築

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import clsx from "clsx";
+import { NextPage } from "next";
+
+import style from "./page.module.scss";
+
+const LifestyleResultShare: NextPage = () => {
+  return <p className={clsx(style["aaaa"])}>1234ABCDEあいうえお</p>;
+};
+
+export default LifestyleResultShare;
