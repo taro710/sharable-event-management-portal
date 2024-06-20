46c30224f82ef05a63133a050cc1c3dcaad0e870
taro710 <katayamayutaro@team-lab.com>
2024-06-20 14:34:53 +0900

    fix: million lint

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import MillionLint from '@million/lint';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default MillionLint.next({
+  rsc: true,
+  optimizeDOM: true,
+})(nextConfig);
